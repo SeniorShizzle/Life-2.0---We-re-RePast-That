239c440a8dfd41a626d024112f1995b8d3471749
SeniorShizzle <SeniorShizzle@gmail.com>
2015-12-01 14:26:36 -0800

    First class

---
+++ src/Life.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Life extends JFrame {
+
+    public static void main(String args[]){
+        new Life().run();
+    }
+
+
+    public void run(){
+        System.out.println("We're aboard!");
+
+    }
+}
