8f142211bd71a0beca762acc6948cc9bab869542
SeniorShizzle <SeniorShizzle@gmail.com>
2015-12-01 14:41:29 -0800

    Runtime loop

--- src/Life.java
+++ src/Life.java
@@ -1,10 +1,14 @@
 import javax.swing.*;
+import javax.swing.plaf.TableHeaderUI;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class Life extends JFrame {
 
     public static final int WINDOW_WIDTH = 1000;
     public static final int WINDOW_HEIGHT = 1000;
+
+    private long tickCount = 0;
 
     public static void main(String args[]){
         new Life().run();
@@ -20,6 +24,7 @@ public class Life extends JFrame {
         // Set the window parameters
         this.setTitle("Life 2.0 — We're RePast That™");
         this.setBackground(new Color(31, 31, 31));
+        this.setResizable(false);
 
         // Set the size of the window
         this.setMinimumSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
@@ -28,5 +33,35 @@ public class Life extends JFrame {
         // Display the window
         this.setVisible(true);
 
+
+
+        //// Begin the runtime loop
+        Timer timer = new Timer(60, ActionEvent -> {
+            System.out.println("tick");
+
+
+            // Update the tick counter
+            tickCount++;
+
+            // Run the update() subroutine
+            update();
+
+            // Repaint the main window
+            repaint();
+
+
+        });
+        timer.start();
+
     }
+
+    public void update(){
+        System.out.println(this.tickCount);
+    }
+
+     
+
+
+
+
 }
