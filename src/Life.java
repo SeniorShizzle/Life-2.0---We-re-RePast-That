ec8ddf948628068fbdc0827a5f28bc831d54ba1f
SeniorShizzle <SeniorShizzle@gmail.com>
2015-12-01 14:32:25 -0800

    Created my first window :')
    To run this, the entry point is in the class Life

--- src/Life.java
+++ src/Life.java
@@ -1,14 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Life extends JFrame {
+
+    public static final int WINDOW_WIDTH = 1000;
+    public static final int WINDOW_HEIGHT = 1000;
 
     public static void main(String args[]){
         new Life().run();
     }
 
 
+    /**
+     * Instantiates and displays the main window. Sets up the window parameters, and initiates the run loop.
+     */
     public void run(){
         System.out.println("We're aboard!");
+
+        // Set the window parameters
+        this.setTitle("Life 2.0 — We're RePast That™");
+        this.setBackground(new Color(31, 31, 31));
+
+        // Set the size of the window
+        this.setMinimumSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        this.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+
+        // Display the window
+        this.setVisible(true);
 
     }
 }
