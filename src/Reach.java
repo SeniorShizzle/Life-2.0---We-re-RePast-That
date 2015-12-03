ce190bc34b17e453d926ba8d024f9a3911b58f5e
af3ld <alexfeldman@lclark.edu>
2015-12-03 14:45:19 -0800

    cleaner?

--- src/Reach.java
+++ src/Reach.java
@@ -5,14 +5,11 @@ public class Reach {
 	public double length;
 	public double sinkX;
 	public double sinkY;
-	public double length;
 	private int reachID;
 	private int nextID;
 
 	public Reach(int reachID, double sourceX, double sourceY, double sinkX,
 			double sinkY, int nextID) {
-<<<<<<< HEAD
-=======
 		this.sourceX = sourceX;
 		this.sourceY = sourceY;
 
@@ -23,22 +20,9 @@ public class Reach {
 		this.nextID = nextID;
 	}
 
-	public double getSourceX() {
-		return sourceX;
-	}
 
-	public void setSourceX(double sourceX) {
->>>>>>> 655925aa2c20dc117f11a0e213ccc691de6441fc
-		this.sourceX = sourceX;
-	}
 
-<<<<<<< HEAD
-		this.sinkX = sinkX;
-		this.sinkY = sinkY;
-		this.length = Math.sqrt(Math.pow(sinkX - sourceX, 2)
-				+ Math.pow(sinkY - sourceY, 2));
-		this.nextID = nextID;
-	}
+
 
 	public double getSourceX() {
 		return sourceX;
@@ -81,51 +65,12 @@ public class Reach {
 	}
 
 	public int getNextID() {
-		return nextID;
-=======
-	public double getSourceY() {
-		return sourceY;
+		return nextID ;
 	}
 
-	public void setSourceY(double sourceY) {
-		this.sourceY = sourceY;
-	}
-
-	public double getSinkX() {
-		return sinkX;
-	}
-
-	public void setSinkX(double sinkX) {
-		this.sinkX = sinkX;
-	}
-
-	public double getSinkY() {
-		return sinkY;
-	}
-
-	public void setSinkY(double sinkY) {
-		this.sinkY = sinkY;
-	}
-
-	public int getReachID() {
-		return reachID;
-	}
-
-	public void setReachID(int reachID) {
-		this.reachID = reachID;
-	}
-
-	public int getNextID() {
-		return nextID;
-	}
-
-	public void setNextID(int nextID) {
-		this.nextID = nextID;
-	}
 
 	public double getLength() {
 		return length;
->>>>>>> 655925aa2c20dc117f11a0e213ccc691de6441fc
 	}
 
 	public void setNextID(int nextID) {
