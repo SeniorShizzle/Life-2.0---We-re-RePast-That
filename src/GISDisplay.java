278271ced231d1f13afd67cbb3d97f35ca16179e
SeniorShizzle <SeniorShizzle@gmail.com>
2015-12-03 15:29:03 -0800

    Reading the GIS File

--- src/GISDisplay.java
+++ src/GISDisplay.java
@@ -1,6 +1,15 @@
+import org.nocrala.tools.gis.data.esri.shapefile.ShapeFileReader;
+import org.nocrala.tools.gis.data.esri.shapefile.header.ShapeFileHeader;
+import org.nocrala.tools.gis.data.esri.shapefile.shape.AbstractShape;
+import org.nocrala.tools.gis.data.esri.shapefile.shape.PointData;
+import org.nocrala.tools.gis.data.esri.shapefile.shape.shapes.MultiPointZShape;
+import org.nocrala.tools.gis.data.esri.shapefile.shape.shapes.PointShape;
+import org.nocrala.tools.gis.data.esri.shapefile.shape.shapes.PolygonShape;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -70,9 +79,57 @@ public class GISDisplay {
         finally {
             setBounds();
             sortReaches();
+
+            try {brokenMethod();} catch (Exception e){}
         }
 
 
+    }
+
+    private void brokenMethod() throws Exception{
+        FileInputStream is = new FileInputStream(
+                "./data/reaches_edt.shp");
+        ShapeFileReader r = new ShapeFileReader(is);
+
+        ShapeFileHeader h = r.getHeader();
+        System.out.println("The shape type of this files is " + h.getShapeType());
+
+        int total = 0;
+        AbstractShape s;
+        while ((s = r.next()) != null) {
+
+            switch (s.getShapeType()) {
+                case POINT:
+                    PointShape aPoint = (PointShape) s;
+                    // Do something with the point shape...
+                    break;
+                case MULTIPOINT_Z:
+                    MultiPointZShape aMultiPointZ = (MultiPointZShape) s;
+                    // Do something with the MultiPointZ shape...
+                    break;
+                case POLYGON:
+                    PolygonShape aPolygon = (PolygonShape) s;
+                    System.out.println("I read a Polygon with "
+                            + aPolygon.getNumberOfParts() + " parts and "
+                            + aPolygon.getNumberOfPoints() + " points");
+                    for (int i = 0; i < aPolygon.getNumberOfParts(); i++) {
+                        PointData[] points = aPolygon.getPointsOfPart(i);
+                        System.out.println("- part " + i + " has " + points.length
+                                + " points.");
+                    }
+                    break;
+                case POLYLINE:
+                    System.out.println("WHAT WHAT");
+                    break;
+                default:
+                    System.out.println("Read other type of shape.");
+            }
+            total++;
+        }
+
+        System.out.println("Total shapes read: " + total);
+
+        is.close();
     }
 
     /**
